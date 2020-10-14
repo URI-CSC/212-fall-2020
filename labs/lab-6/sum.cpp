247da738680798d3ff634b1c2d35676e450a6687
Christian Esteves <c.esteves92@gmail.com>
2020-10-14 12:25:08 -0400

    Create sum.cpp

---
+++ labs/lab-6/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+unsigned long sum(unsigned int n) {
+    // Start entering code here
+    return 0;
+}
+
+int main() {
+    std::cout << sum(10) << std::endl;
+    std::cout << sum(100) << std::endl;
+}
